9b610412817d28678cc0f14ac1c896bdd64e6db1
Sudharsan <51089246+Sudharsan93@users.noreply.github.com>
2023-09-06 09:57:56 +0530

    Update  2 aws.tf

--- aws.tf
+++ aws.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "sudharsan-webserver" {
     ami = "ami-051f7e7f6c2f40dc1"
     instance_type = "t2.micro"
     tags = {
-        Name = "Sudharsan-webserver"
+        Name = "Sudharsan-webserver1"
     }
 }
